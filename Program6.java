eda50ff07709b9bbe2e30164f91493351f37084c
Morgan428 <MorganWare428@gmail.com>
2024-08-19 08:45:58 -0700

    Update Program6.java
    Needs to be complied

--- Program6.java
+++ Program6.java
@@ -1,7 +1,19 @@
-//import stuff here!
-
-//Your code here
-
+import java.util.Scanner;
+public class Program6{
+  public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    static double p = 3.14159;
+    System.out.println("Input Radius:");
+    double r = s.nextline();
+    double d = r + r;
+    double a = r * r * p;
+    double c = p * d;
+    System.out.println("The Radius of the circle = " + r);
+    System.out.println("The Diametet of the circle = " + d);
+    System.out.println("The Area of the circle = " + a);
+    System.out.println("The Circumference of the circle = " + c);
+  }
+}
 //Paste console output below:
 /*
 
